3a0cbadd0f49fd217e46670505a7a2ffac828cc6
Renia Dioleti <reniadiol3@gmail.com>
2023-06-12 15:36:07 +0300

    Update StudentController.java

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
-
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
@@ -15,12 +14,18 @@ public class StudentController {
 
     @Autowired
     public StudentController(StudentService studentService) {
-        this.studentService = studentService; // Update the field name to "this.studentService"
+        this.studentService = studentService; // Initialize the studentService field with the provided StudentService instance
     }
 
+    /**
+     * Get a list of all students.
+     *
+     * @return List of Student objects
+     */
     @GetMapping
     public List<Student> getStudents() {
-        return studentService.getStudents();
+        return studentService.getStudents(); // Retrieve the list of students from the studentService
     }
 }
+
 
